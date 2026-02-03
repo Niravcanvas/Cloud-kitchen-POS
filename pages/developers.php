27db0ddfb0d4c3c2e338b9464ec4793fdb9a6c95
Nirav Thakur <niravthakur2005@gmail.com>
2026-02-03 20:01:30 +0530

    added pictures in developers page

--- pages/developers.php
+++ pages/developers.php
@@ -321,7 +321,7 @@ session_start();
         <!-- Developer 2: Nirav -->
         <div class="developer-card">
           <div class="developer-photo-wrapper">
-            <img src="static/nirav.jpg" alt="Nirav Thakur" class="developer-photo">
+            <img src="../assets/images/N1.jpg" alt="Nirav Thakur" class="developer-photo">
             <div class="photo-badge">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42" />
@@ -353,7 +353,7 @@ session_start();
         <!-- Developer 3: Rushab -->
         <div class="developer-card">
           <div class="developer-photo-wrapper">
-            <img src="static/rushab.jpg" alt="Rushab Nhaikade" class="developer-photo">
+            <img src="../assets/images/R1.jpg" alt="Rushab Nhaikade" class="developer-photo">
             <div class="photo-badge">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z" />
